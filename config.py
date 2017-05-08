eb348c73488a9c2d147e5de1aed84a5778cfa26f
pshenglh <psheng@outlook.com>
2017-05-08 20:05:06 +0800

    toolbar集成

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 SECRET_KEY = 'anxious'
 SQLALCHEMY_DATABASE_URI = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 UPLOAD_FOLDER = UPLOAD_FOLDER
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
--- manage.py
+++ manage.py
@@ -3,10 +3,17 @@ from flask_script import Shell, Manager
 from app import create_app, db
 from app.models import Admin, Post, Comment
 from flask_migrate import Migrate
+import os
 
 app = create_app()
 manager = Manager(app)
 migrate = Migrate(app, db)
+debug = os.getenv('DEBUG')
+
+if debug:
+    from flask_debugtoolbar import DebugToolbarExtension
+    app.debug = True
+    toolbar = DebugToolbarExtension(app)
 
 def make_shell_context():
     return dict(app=app, db=db, Admin=Admin, Post=Post,
@@ -16,4 +23,3 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
-    app.run(debug=True)
