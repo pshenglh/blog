a96889ef37c78941e96b035a7a232c3c87893c57
pshenglh <psheng@outlook.com>
2017-08-22 12:53:11 +0800

    查看更多修改

--- app/static/more.js
+++ app/static/more.js
@@ -1,19 +1,25 @@
 $(function () {
     var $more = $("#more");
-    $more.click(function () {
+    var $pages = $more.attr("pages");
+    $more.bind("click", function () {
         var page = $(this).attr("value");
         $.get($SCRIPT_ROOT + '/more', {
             page : page
         }, function (data, textStatus) {
                 $(".article:last").after(data);
                 var $next_page = parseInt(page)+1;
+                if ($next_page > $pages){
+                    $more.text("已显示所有内容");
+                    $more.unbind("click")
+                    $more.unbind("mouseover")
+                }
                 $more.attr("value", parseInt(page)+1);
         });
     });
-    $more.mouseover(function () {
+    $more.bind("mouseover", function () {
         $(this).css("background-color", "#E0E0E0")
     });
-    $more.mouseout(function () {
+    $more.bind("mouseout", function () {
         $(this).css("background-color", "white")
     });
 });
